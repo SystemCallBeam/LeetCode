a19fecb97a94c47283f4f59064b15e33fbfd2d7e
SystemCallBeam <tkhaeamadan@gmail.com>
2023-10-04 19:51:16 +0700

    edit put()

--- MyHashMap.java
+++ MyHashMap.java
@@ -1,13 +1,23 @@
 class MyHashMap {
     int MAX_SIZE = 1000000;
-    int[] key = new int[MAX_SIZE]
+    int[] key = new int[MAX_SIZE];
+    int[] value = new int[MAX_SIZE];
+    int i = 0; // start
 
     public MyHashMap() {
         
     }
     
     public void put(int key, int value) {
-        
+        do {
+            if (key == this.key[i]) {
+                this.value[i] = value;
+            }
+        } while (i < MAX_SIZE && this.key[++i] != 0);
+        if (this.key[i] == 0) {
+            this.key[i] = key;
+            this.value[i] = value;
+        }
     }
     
     public int get(int key) {
