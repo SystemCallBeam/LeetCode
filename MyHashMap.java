8447d4c7476566fb2a8c6bcd7400249ee0859aee
SystemCallBeam <tkhaeamadan@gmail.com>
2023-10-04 21:38:10 +0700

    edit get()

--- MyHashMap.java
+++ MyHashMap.java
@@ -2,7 +2,8 @@ class MyHashMap {
     int MAX_SIZE = 1000000;
     int[] key = new int[MAX_SIZE];
     int[] value = new int[MAX_SIZE];
-    int i = 0; // start
+    int size = 0; // start
+    int idx = 0;
 
     public MyHashMap() {
         
@@ -10,18 +11,28 @@ class MyHashMap {
     
     public void put(int key, int value) {
         do {
-            if (key == this.key[i]) {
-                this.value[i] = value;
+            if (key == this.key[]) {
+                this.value[idx] = value;
             }
-        } while (i < MAX_SIZE && this.key[++i] != 0);
-        if (this.key[i] == 0) {
-            this.key[i] = key;
-            this.value[i] = value;
+        } while (idx < MAX_SIZE && this.key[++idx] != 0);
+        if (this.key[idx] == 0) {
+            this.key[idx] = key;
+            this.value[idx] = value;
+        }
+        for (int i = 0; i < MAX_SIZE; i++) {
+            
         }
     }
     
     public int get(int key) {
-        
+        for (int i = 0; i < MAX_SIZE; i++) {
+            if (key == this.key[i]) {
+                return value[i];
+            } else if (this.key[i] == 0) {
+                return -1;
+            }
+        }
+        return -1;
     }
     
     public void remove(int key) {
