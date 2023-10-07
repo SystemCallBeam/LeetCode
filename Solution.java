8333e23a7d3b7ba4f614ba681dfd6aa949b42719
SystemCallBeam <tkhaeamadan@gmail.com>
2023-10-07 14:30:52 +0700

    reversion and complete v2

--- Solution.java
+++ Solution.java
@@ -1,13 +1,17 @@
+import java.util.HashMap;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    int ans[] = {i, j};
-                    return ans;
-                }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            if (map.containsKey(temp) && map.get(temp) != i) {
+                return new int[]{i, map.get(temp)};
             }
-        }        
+        }
         return null;
     }
 }
